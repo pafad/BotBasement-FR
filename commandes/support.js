814c7f37a7edcf266bcd7ed86e07be41ecd98063
pafad <32877302+pafad@users.noreply.github.com>
2018-02-20 00:28:25 +0100

    Update support.js

--- commandes/support.js
+++ commandes/support.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
     if (!args) return message.channel.send("** ❌ | Merci de me dire le bug ou la question**");
                 message.channel.send("**☑ Votre report a été envoyé** : " + args)
-            client.channels.get("414592429161840663").send({embed:{ color: 0x002AD1,
+            client.channels.get(" 415288257870430208").send({embed:{ color: 0x002AD1,
             author:{
             name: "Support du bot",
             icon_url: client.avatarUrl
