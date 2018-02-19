ca3712f6af31b024a06066d306154e9109c22060
pafad <32877302+pafad@users.noreply.github.com>
2018-02-20 00:53:42 +0100

    Update support.js

--- commandes/support.js
+++ commandes/support.js
@@ -1,24 +1,24 @@
 exports.run = (client, message, args) => {
     if (!args) return message.channel.send("** ❌ | Merci de me dire le bug ou la question**");
                 message.channel.send("**☑ Votre report a été envoyé** : " + args)
-            client.channels.get("415288257870430208").send('' , { embed: { 
-                color: 0x002AD1,
-            author:{
-            name: "Support du bot",
-            icon_url: client.user.avatarUrl,
-            }, 
-            title: '',
-            url: '',
-            fields:[
-                {
-                name:"question/bug rapporté",
-                value: args,
-                inline:false
-                },
-                ],
-            footer:{
-            icon_url: message.author.avatarUrl,
-            text: `une question/bug de ${message.author.username}.`
-            },
+            client.channels.get("415288257870430208").send('', { embed: {
+      color: 0x002AD1,
+      author: {
+        name: message.author.tag,
+        icon_url: message.author.avatarURL,
+      },
+      title: '',
+      url: '',
+      fields: [
+        {
+          name: 'Question/bug',
+          value: `${args}`,
+          inline: true
+        },
+      ],
+      footer: {
+        icon_url: client.user.avatarURL,
+        text: client.user.username + " | questions/bug by shiro."
+      },
     }})
 }
