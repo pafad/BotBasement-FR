6311fa6a2f186890fbf4ba9b8da1d43e98a47f02
pafad <32877302+pafad@users.noreply.github.com>
2018-02-19 23:21:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -81,5 +81,17 @@ if(message.content.startsWith(prefix + "setavatar")){
             return;
             }
         }
+    //setpseudo
+        if(message.content === prefix + "setpseudo"){
+            if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
+                if (!args) return message.channel.send("** ❌ | Vous n'avez rien noté.**");
+                    message.delete();
+                    client.user.setUsername(`${args}`)
+                    message.channel.send("** :white_check_mark: | Pseudo changé !**").catch(console.error)
+                }else{
+                message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
+                return;
+                }
+        }
 });
 client.login(process.env.Discord_token || process.agrv[2]);
