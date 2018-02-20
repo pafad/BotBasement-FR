dbdbdab91cd8c4bde8f15732e6433392390fc0fa
pafad <32877302+pafad@users.noreply.github.com>
2018-02-20 01:05:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -30,18 +30,6 @@ client.on('message', message => {
   } catch (err){
   return;
   }
-    //setpseudo
-        if(message.content === prefix + "setpseudo"){
-            if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
-                if (!args) return message.channel.send("** ❌ | Vous n'avez rien noté.**");
-                    message.delete();
-                    client.user.setUsername(`${args}`)
-                    message.channel.send("** :white_check_mark: | Pseudo changé !**").catch(console.error)
-                }else{
-                message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
-                return;
-                }
-            }
             //reload
             if(message.content === prefix + "reload"){
             if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
