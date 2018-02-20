a59bcf828ed6a7dec187cb4d2c0e7f35b0939c5d
pafad <32877302+pafad@users.noreply.github.com>
2018-02-20 01:03:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -30,17 +30,6 @@ client.on('message', message => {
   } catch (err){
   return;
   }
-    if(message.content.startsWith(prefix + "setgame")){
-        if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
-            if (!args) return message.channel.send("** ❌ | Vous n'avez rien noté.**");
-                message.delete();
-                client.user.setPresence({game:{name: `#bot-list || ${args}`, url:"https://www.twitch.tv/pafad0gaming",type}});
-                message.channel.send("** :white_check_mark: | Jeu changé !**").catch(console.error)
-            }else{
-            message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
-            return;
-            }
-        }
     //setpseudo
         if(message.content === prefix + "setpseudo"){
             if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
