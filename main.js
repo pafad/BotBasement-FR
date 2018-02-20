4e632b3877ed8389f5b1b5e5eb02bd0d5cfd8bd8
pafad <32877302+pafad@users.noreply.github.com>
2018-02-20 01:09:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -30,20 +30,7 @@ client.on('message', message => {
   } catch (err){
   return;
   }
-            //reload
-            if(message.content === prefix + "reload"){
-            if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
-                message.channel.send("** :white_check_mark: | mise à jour...**")    
-                client.destroy();
-                console.log("déco")
-                client.login(process.env.Discord_token || process.agrv[2]);
-                console.log("reco")
-                message.channel.send("** :white_check_mark: | mise à jour terminée...**")
-                }else{
-                message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
-                return;
-                }
-        }
+}
     
 });
 client.login(process.env.Discord_token || process.agrv[2]);
