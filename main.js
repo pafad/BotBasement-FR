1ba8ca1ab49250c9da2ecfcf4ece6fb4d7b1b0d1
pafad <32877302+pafad@users.noreply.github.com>
2018-02-20 01:02:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -30,18 +30,6 @@ client.on('message', message => {
   } catch (err){
   return;
   }
-//setavatar
-if(message.content.startsWith(prefix + "setavatar")){
-    if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
-        if (!args) return message.channel.send("** ❌ | Vous n'avez rien noté.**");
-            message.delete();
-            client.user.setAvatar(args);
-            message.channel.send("** :white_check_mark: | Avatar changé !**").catch(console.error)
-        }else{
-        message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
-        return;
-        }
-    }
     if(message.content.startsWith(prefix + "setgame")){
         if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
             if (!args) return message.channel.send("** ❌ | Vous n'avez rien noté.**");
