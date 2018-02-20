7a61c826d32cb4bcb98bb95731946dd51124dfa5
pafad <32877302+pafad@users.noreply.github.com>
2018-02-21 00:10:58 +0100

    Update setgame.js

--- commandes/setgame.js
+++ commandes/setgame.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-if(message.author.id !== "317221808405348364" || message.author.id !== "169146903462805504" || message.author.id !== "306119836503900161"){
+if(!message.author.id === "317221808405348364" || !message.author.id === "169146903462805504" || !message.author.id === "306119836503900161"){
   message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
   return;
 }else{
