15deae07244afb1f5c239226520e59d574353dd6
pafad <32877302+pafad@users.noreply.github.com>
2018-02-21 01:08:27 +0100

    Update setgame.js

--- commandes/setgame.js
+++ commandes/setgame.js
@@ -3,10 +3,13 @@ if(!message.author.id === "317221808405348364" || !message.author.id === "169146
   message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
   return;
     }else{
-  if(!args) return message.channel.send(":x: jeu indéfini veuillez noter quelque chose.");
+  if(!args){
+    message.channel.send(":x: jeu indéfini veuillez noter quelque chose.");
+  }else{
   client.user.setPresence({game:{name: `#bot-list || ${message.content.substr(9)}`, url:"https://www.twitch.tv/pafad0gaming",type}})
   message.delete();
   message.channel.send("** :white_check_mark: | Jeu changé !**")
   message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
+        }
     }
 }
