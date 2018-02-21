177f861a9c14644c2a13cd8a7a20a68189a9f55d
pafad <32877302+pafad@users.noreply.github.com>
2018-02-21 10:37:29 +0100

    Update setgame.js

--- commandes/setgame.js
+++ commandes/setgame.js
@@ -1,12 +1,22 @@
 exports.run = (client, message, args) => {
-    if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
-      message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
-      return;
+    if(message.author.id !== "317221808405348364"){
+       message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
+       return;
     }else{
+        if(message.author.id !== "169146903462805504"){
+            message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
+            return;
+        }else{
+            if(message.author.id !== "306119836503900161"){
+                message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
+                return;   
+            }else{
       if(!args) return message.channel.send(":x: jeu indéfini veuillez noter quelque chose.");
       message.delete();
       client.user.setPresence({game:{name: `#bot-list || ${args}`, url:"https://www.twitch.tv/pafad0gaming",type}})
       message.delete();
       message.channel.send("** :white_check_mark: | Jeu changé !**")
+            }
+        }
     }
  }
