252d4e277f90a9a39e0eece33904930eb31b8408
pafad <32877302+pafad@users.noreply.github.com>
2018-08-28 19:45:17 +0000

    Update reload.js

--- commandes/reload.js
+++ commandes/reload.js
@@ -1,9 +1,9 @@
 exports.run = (client, message) => {
-if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
+if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "377925283098918912"){
          message.channel.send("** :white_check_mark: | mise à jour...**")    
          client.destroy();
          console.log("déco")
-         client.login(process.env.Discord_token || process.agrv[2]);
+         client.login(process.env.token);
          console.log("reco")
          message.channel.send("** :white_check_mark: | mise à jour terminée...**")
             }else{
