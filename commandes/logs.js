de87190f3a80609ef08569708d1c1b4f3b74f5d9
pafad <32877302+pafad@users.noreply.github.com>
2018-08-28 19:44:31 +0000

    Update logs.js

--- commandes/logs.js
+++ commandes/logs.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 let usermention = message.mentions.users.first()
-if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "306119836503900161"){
+if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "377925283098918912"){
     if (!args) return message.channel.send("** ❌ | Vous n'avez rien noté.**");
     if(!usermention) return message.channel.send("** ❌ spécifiez une mention d'un bot.**")
          message.delete();
