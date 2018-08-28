ef1e57ecb17d0c5b26565af15357d7c03bfdeac2
pafad <32877302+pafad@users.noreply.github.com>
2018-08-28 20:00:08 +0000

    Update setpseudo.js

--- commandes/setpseudo.js
+++ commandes/setpseudo.js
@@ -1,8 +1,7 @@
 exports.run = (client, message, args) => {
 if(message.author.id === "317221808405348364" || message.author.id === "169146903462805504" || message.author.id === "377925283098918912"){
-if (!args) return message.channel.send("** ❌ | Vous n'avez rien noté.**");
            message.delete();
-           client.user.setUsername(`${args} Bots-List`)
+           client.user.setUsername(`${message.guild.name} Bots-List`)
            message.channel.send("** :white_check_mark: | Pseudo changé !**")
               }else{
            message.channel.send(`:x: | ${message.author} tu n'est pas dans le support du bot.`)
